c9824b1cc5533d5ac96ccf17da5bb114b3b48e23
tiyang0110 <tiyang0110@douzone.com>
2024-10-09 11:03:46 +0900

    TailwindCSS 3.10할차례

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm dark:bg-gray-600 flex flex-col gap-2 md:flex-row">
-        
-        <input className="w-full rounded-full py-3 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow" type="text" placeholder="Search here.."/>
-        <button className="bg-black text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none md:px-10">Search</button>
-
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm dark:bg-gray-600 flex flex-col gap-3">
+        {['ti', 'me', 'you', 'your self'].map((person, i) => {
+          return (
+            <div key={i} className="flex items-center gap-5 group">
+              <div className="size-10 bg-blue-400 rounded-full"/>
+              <span className="text-lg font-medium group-hover:text-red-500">{person}</span>
+              <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full text-sm relative">
+                <span className="z-10">{i}</span>
+                <div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
+              </div>
+            </div>
+          );
+        })}
       </div>
     </main>
   );

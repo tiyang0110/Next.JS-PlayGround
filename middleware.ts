fe18b430d8fa1ab407a4304d8f84029f6b388f30
tiyang0110 <tiyang0110@douzone.com>
2024-10-21 09:04:50 +0900

    중간 커밋

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
 
 export async function middleware(request: NextRequest){
-  const session = await getSession();
   
-  if(request.nextUrl.pathname === '/profile'){
-    return Response.redirect(new URL('/', request.url));
-  }
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
 }

0c6e629874298bc1e67ddcba14b38bdfc758f54c
tiyang0110 <tiyang0110@douzone.com>
2024-11-01 16:49:22 +0900

    12.7 modal 코드 챌린지 해보자

---
+++ app/(tabs)/products/@modal/(...)products/[id]/loading.tsx
@@ -0,0 +1,3 @@
+export default function ProductDetailModalLoading(){
+  return null;
+}
--- app/(tabs)/products/@modal/(...)products/[id]/page.tsx
+++ app/(tabs)/products/@modal/(...)products/[id]/page.tsx
@@ -1,5 +1,29 @@
-export default function ProductDetailModal(){
+"use client";
+
+import Link from "next/link";
+import { PhotoIcon, XMarkIcon } from "@heroicons/react/24/solid"
+import { useRouter } from "next/navigation";
+
+export default async function ProductDetailModal({
+  params
+}:{
+  params: { id: string }
+}){
+  const router = useRouter();
+  const onCloseClick = () => {
+    router.back();
+  }
+
   return (
-    <span>hi modal</span>
+    <div className="absolute w-full h-full z-50 flex justify-center items-center bg-black bg-opacity-60 left-0 top-0">
+      <button onClick={onCloseClick} className="absolute right-20 top-20">
+        <XMarkIcon className="size-10" />
+      </button>
+      <div className="max-w-screen-sm w-full">
+        <div className="text-neutral-200 aspect-square bg-neutral-700 rounded-md flex justify-center items-center">
+          <PhotoIcon className="h-28" />
+        </div>
+      </div>
+    </div>
   )
 }
